6dcbde170e610966d3ddb3748285a2dab7d764be
ISA <iqro.s.ahfadz@gmail.com>
2025-01-20 12:02:58 +0700

    almost finish menu layout

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function createMenu(dishName, imageURL, dishDesc){
     
     target.innerHTML += 
     NewElement("div", "class=\"menu\"",
-        NewElement("img", `src="${imageURL}" alt="dish"`, false)+
+        NewElement("img", `src="assets/img/img_default.jpeg" alt="dish"`, false)+
         NewElement("div", "class=\"menu-content\"",
-            NewElement("h1", null, "Menu")+
-            NewElement("p", null, "Menu Description")
+            NewElement("h1", null, dishName)+
+            NewElement("p", null, dishDesc)
         )+
         NewElement("div", "class=\"menu-button\"",
             NewElement("span", null, "Info!")+
@@ -27,10 +27,17 @@ function createMenu(dishName, imageURL, dishDesc){
     );
 }
 
-
+let dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut "+
+                "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "+
+                "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit "+
+                "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, "+
+                "sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-createMenu("Nasi Goreng", "assets/img/curry_1.jpeg", "Indonesian Fried Rice");
+createMenu("Nasi Goreng", "assets/img/curry_1.jpeg", dummyText);
+createMenu("Soto", "assets/img/curry_1.jpeg", dummyText);
+createMenu("Empal Gentong", "assets/img/curry_1.jpeg", dummyText);
+createMenu("Nila Bakar", "assets/img/curry_1.jpeg", dummyText);
 
 });
